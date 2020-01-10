92e49b4dc99861ade3b94941137705727cdc27cf
robertoravec141 <roravec141@gmail.com>
2020-01-10 11:05:34 +0100

    added few models, updated gitignore

---
+++ SISimulation/Model/Category.swift
@@ -0,0 +1,13 @@
+//
+//  Category.swift
+//  SISimulation
+//
+//  Created by Róbert Oravec on 10/01/2020.
+//  Copyright © 2020 Robo. All rights reserved.
+//
+
+import Foundation
+
+enum Category {
+    
+}
---
+++ SISimulation/Model/Field.swift
@@ -0,0 +1,16 @@
+//
+//  Field.swift
+//  SISimulation
+//
+//  Created by Róbert Oravec on 10/01/2020.
+//  Copyright © 2020 Robo. All rights reserved.
+//
+
+import Foundation
+
+enum Field {
+    case finance
+    case cestovnyRuch
+    case ekonomickaInformatika
+    case zahranicniStudenti
+}
