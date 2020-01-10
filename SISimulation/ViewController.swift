dbe468967b89304cee45f0cfab582581c7f936b8
robertoravec141 <roravec141@gmail.com>
2020-01-10 10:54:49 +0100

    created project, initial commit

---
+++ SISimulation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SISimulation
+//
+//  Created by Róbert Oravec on 10/01/2020.
+//  Copyright © 2020 Robo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
